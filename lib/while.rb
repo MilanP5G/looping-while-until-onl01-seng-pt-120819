cfa9abaf72567c97407a46af6ed5a80f7690ab34
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-11 17:46:22 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,12 +2,9 @@ def using_while
   levitation_force = 6
 	
   levitation_force < 10
-  num_of_hotdogs_eaten += 1
-  puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+  levitation_force += 1
+  puts "Wingardium Leviosa"
 end
-# => nil (return value)
  
-puts "You ate a total of #{num_of_hotdogs_eaten} hot dogs!"
-end
 
 
